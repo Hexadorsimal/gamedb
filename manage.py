cb189326894ffc4794c399178129b7a4d914e672
John Whipple <john@whipple.org>
2017-02-19 16:41:47 -0500

    added flask migrate

--- manage.py
+++ manage.py
@@ -1,3 +1,4 @@
+from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager, Server, Shell
 
 from gamedb import app, db
@@ -7,10 +8,12 @@ from gamedb.model import Role, User
 def _make_context():
     return dict(app=app, db=db, User=User, Role=Role)
 
+migrate = Migrate(app, db)
 
 manager = Manager(app)
 manager.add_command("runserver", Server(host="0.0.0.0", port=2600))
 manager.add_command("shell", Shell(make_context=_make_context))
+manager.add_command("db", MigrateCommand)
 
 
 if __name__ == "__main__":
