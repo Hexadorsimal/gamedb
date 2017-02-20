6859a36ca47255c24f360e241cba667a7ca18e66
John Whipple <john@whipple.org>
2017-02-19 20:11:31 -0500

    Added Flask-Mail

--- gamedb/__init__.py
+++ gamedb/__init__.py
@@ -1,5 +1,8 @@
+import os
+
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from flask_mail import Mail
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 
@@ -9,7 +12,14 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://gamer:password@localhost/g
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = "hard to guess string"
 
+app.config["MAIL_SERVER"] = "smtp.googlemail.com"
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME")
+app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
+
 bootstrap = Bootstrap(app)
+mail = Mail(app)
 moment = Moment(app)
 db = SQLAlchemy(app)
 
