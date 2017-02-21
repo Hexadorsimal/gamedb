aa9d1a387b29cdc97571efd50badf5d0f2296fcc
John Whipple <john@whipple.org>
2017-02-21 16:55:20 -0500

    Added a password_hash to the user model. Added unit tests for the user.

--- gamedb/model/user.py
+++ gamedb/model/user.py
@@ -1,3 +1,5 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+
 from gamedb import db
 
 
@@ -7,6 +9,18 @@ class User(db.Model):
     email = db.Column(db.String(), unique=True)
     role_id = db.Column(db.Integer, db.ForeignKey("role.id"))
     role = db.relationship("Role")
+    password_hash = db.Column(db.String(128))
 
     def __repr__(self):
         return "<User {name}>".format(name=self.name)
+
+    @property
+    def password(self):
+        raise AttributeError("password is not a readable attribute")
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
