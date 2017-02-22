f1ea67468b9ed82059d2e00ea437c5fe53e51fc5
John Whipple <john@whipple.org>
2017-02-22 16:39:24 -0500

    Added a bunch of login stuff, but the emails don't work.

--- config.py
+++ config.py
@@ -8,6 +8,11 @@ class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "hard to guess string"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    MAIL_SERVER = os.environ.get("MAIL_SERVER")
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     FLASKY_MAIL_SUBJECT_PREFIX = "[Flasky]"
     FLASKY_MAIL_SENDER = "Flasky Admin <flasky@example.com>"
     FLASKY_ADMIN = os.environ.get("FLASKY_ADMIN")
@@ -19,11 +24,6 @@ class Config(object):
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    MAIL_SERVER = "smtp.googlemail.com"
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     SQLALCHEMY_DATABASE_URI = "postgresql://gamer:password@localhost/dev-gamedb"
 
 
